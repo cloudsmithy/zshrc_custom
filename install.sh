a19b740ec09fb1fb209156968fa42be3c7bd4b7e
cloudsmithy <deep.work.max@gmail.com>
2025-07-29 15:49:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 brew update && brew upgrade
-brew install wget lazygit bat cloudflared htop tree ffmpeg fastfetch dust kubectl
+brew install wget lazygit bat cloudflared htop tree ffmpeg fastfetch dust kubectl httpie
 brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install --cask slack
